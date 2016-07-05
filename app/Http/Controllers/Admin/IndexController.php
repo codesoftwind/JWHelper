0123c7ac42e04da3ec805cd105ff639a85867712
wangdong <buaawangdong@163.com>
2016-07-05 18:04:52 +0800

    finish teacher publishing homework and change the routes

--- app/Http/Controllers/Admin/IndexController.php
+++ app/Http/Controllers/Admin/IndexController.php
@@ -12,7 +12,7 @@ use Illuminate\Database\Eloquent\Model;
 class IndexController extends Controller {
 
 	/**
-	 * 显示教务首页
+	 * 显示教务首页，默认显示课程列表
 	 */
 	public function index()
 	{
@@ -22,15 +22,15 @@ class IndexController extends Controller {
 		$adminID = Auth::user()->userID;
 		$admin = DB::table('admins')
 								->where('adminID', $adminID)
-								->select('adminID', 'adminName')
+								->select('adminName')
 								->get();
 		$tmpresult = DB::table('tlessons')
 								->join('lessons	', 'tlessons.lessonID', '=', 'lessons.lessonID')
 								->join('teachers', 'tlessons.teacherID', '=', 'teachers.teacherID')
 								->select('lessons.lessonID', 'lessons.lessonName', 'teachers.teacherID', 'teachers.teacherName')
 								->get();
-		$result = ['adminID'=>$admin[0]['adminID'], 'adminName'=>$admin[0]['adminName'], 'result'=>$tmpresult];
-		return response()->json($result);
+		$result = ['title'=>'课程列表', 'adminName'=>$admin[0]['adminName'], 'role'=>'教务管理员', 'result'=>$tmpresult];
+		return view('view.admin.index')->with($result);
 	}
 	
 
