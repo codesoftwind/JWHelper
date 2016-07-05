04a2ab082fddcbe8e2a8954c7a9de5e05018c30f
周子尧 <569175788@qq.com>
2016-07-05 17:25:08 +0800

    周子尧
    课程列表

--- resources/views/view/admin/index.blade.php
+++ resources/views/view/admin/index.blade.php
@@ -1,1 +1,45 @@
 @extends('view.template.admin_layout')
+
+@section('main_panel')
+<div class="page-header">
+  <h3>课程列表</h3>
+</div>
+<div class="table-responsive">
+<table class="table table-striped">
+ <tr>
+    <th>课程</th>
+    <th>教师</th>
+    <th></th>
+    <th></th>
+  </tr>
+
+        <?php 
+          
+          //$lesson = $result;
+          //$lesson['lessonName']
+        ?>
+
+       @foreach ($lesson as $lesson)
+           <tr> <td>{{  $lesson[0] }}
+           </td>
+
+           <td>{{  $lesson[1] }}</td>      
+<td>
+  <div class="btn-group" role="group" aria-label="...">
+  <button type="button" class="btn btn-primary" >修改</button>
+</div>
+</td>
+<td>
+  <div class="btn-group" role="group" aria-label="...">
+  <button type="button" class="btn btn-default" >删除</button>
+</div>
+</td>
+           </tr>  
+       @endforeach
+
+
+
+</table>
+</div>
+
+@endsection
