7218f4275dce152d7bb3925984e5ba4b98b58af5
Leo Peng <pengzimaobuaa@163.com>
2016-07-08 15:27:43 +0800

    添加学生团队审核界面

--- resources/views/view/student/checkGroup.blade.php
+++ resources/views/view/student/checkGroup.blade.php
@@ -1,6 +1,6 @@
 @extends('view.student.group_layout')
 
-@section('header', '我的团队')
+@section('header', '审核团队')
 
 @section('main_content')
     <?php
@@ -15,36 +15,25 @@
     <table class="table">
         <thead>
         <tr>
+            <th style="color:#55595c;background-color:#eceeef">申请人姓名</th>
             <th style="color:#55595c;background-color:#eceeef">团队名称</th>
-            <th style="color:#55595c;background-color:#eceeef">团队负责人</th>
             <th style="color:#55595c;background-color:#eceeef">团队人数上限</th>
             <th style="color:#55595c;background-color:#eceeef">团队现有人数</th>
-            <th style="color:#55595c;background-color:#eceeef">审核(申请人数)</th>
-            <th style="color:#55595c;background-color:#eceeef">操作</th>
+            <th style="color:#55595c;background-color:#eceeef"></th>
+            <th style="color:#55595c;background-color:#eceeef"></th>
         </tr>
         </thead>
         <tbody>
         @foreach($ingroups as $group)
             <tr>
-                <th>{{$group->groupName}}</th>
                 <th>{{$group->headName}}</th>
+                <th>{{$group->groupName}}</th>
                 <th>{{$group->maxPeople}}</th>
                 <th>{{$group->occupied}}</th>
-                <th><button class="btn btn-primary check">审核<span> ( {{$group->applyPeople}} ) </span></button></th>
-                <th><button class="btn btn-danger">退出团队</button></th>
+                <th><button class="btn btn-success">同意</button></th>
+                <th><button class="btn btn-danger">拒绝</button></th>
             </tr>
         @endforeach
         </tbody>
     </table>
-@endsection
-
-@section('bodyJS')
-    @parent
-    <script>
-        $(function () {
-            $(".check").click(function () {
-                window.location.href = "http://localhost/JWHelper/public/student/checkGroup";
-            });
-        });
-    </script>
 @endsection
