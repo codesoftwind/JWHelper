0d5dbba6add4ed7c13e2360c3ac2524acf5648c8
wzy <596913416@qq.com>
2016-07-09 16:57:34 +0800

    作业详情页面

--- resources/views/view/student/homeworkdetail.blade.php
+++ resources/views/view/student/homeworkdetail.blade.php
@@ -3,7 +3,7 @@
 @section('main_panel')
 
         <?php 
-          class Group{
+          class homework{
             var $groupName;
             var $leaderName;
             var $groupMember;
@@ -12,13 +12,13 @@
             var $backPage;
           }
 
-          $group=new Group();
-          $group->groupID='123456';
-          $group->groupName='大刀队';
-          $group->leaderName='张大彪';
-          $group->groupMember=['老王','张嘎子','徐逸','张宝华','聂戴琳','张三','李四','王五','董建华'];
-          $group->lessonID='666';
-          $group->backPage='accept';
+          $homework=new homework();
+          $homework->homeworkID='123456';
+          $homework->homeworkScore='90';
+          $homework->teachercomment='不错';
+          $homework->homeworkrequire='要认真';
+          $homework->homeworkDetail='666啦啦啦啦啦啦啦啦啦啦啦啦啦啦了啦啦啦啦啦';
+          $homework->homeworklink='baidu';
 
         ?>
 <div>
@@ -30,28 +30,25 @@
 
   <tr>
     <th width="100">作业分数</th>
-    <td>{{$group->groupName}}</td>
+    <td>{{$homework->homeworkScore}}</td>
   </tr>
   
   <tr><td>&nbsp</td></tr>
    <tr>
     <th width="100">教师评价</th>
-    <td>{{$group->groupName}}</td>
+    <td>{{$homework->teachercomment}}</td>
   </tr>
   
   <tr><td>&nbsp</td></tr>
    <tr>
-    <th width="100">作业详情</th>
-    <td>{{$group->groupName}}
-Bootstrap 提供了下列类型的表单布局：
-垂直表单（默认）
-内联表单
-水平表单
-垂直或基本表单
-基本的表单结构是 Bootstrap 自带的，个别的表单控件自动接收一些全局样式。下面列出了创建基本表单的步骤：
-向父 元素添加 role="form"。
-把标签和控件放在一个带有 class .form-group 的  中。这是获取最佳间距所必需的。
-向所有的文本元
+    <th width="100">作业要求</th>
+    <td>{{$homework->homeworkrequire}}</td>
+  </tr>
+  
+  <tr><td>&nbsp</td></tr>
+   <tr>
+    <th width="100">上传作业详情</th>
+    <td>{{$homework->homeworkDetail}}
     </td>
   </tr>
   
@@ -59,7 +56,7 @@ Bootstrap 提供了下列类型的表单布局：
   <tr>
     <th width="100">附件下载</th>
     <td>
-    <a href="链接" target="_blank"><input type="button" class="btn  btn-primary" value="点击下载"></input></a>
+    <a href="{{$homework->homeworklink}}" target="_blank"><input type="button" class="btn  btn-primary" value="点击下载"></input></a>
     </td>
   </tr>
   <tr><td>&nbsp</td></tr>
@@ -67,15 +64,10 @@ Bootstrap 提供了下列类型的表单布局：
 </table>
   <tr><td>&nbsp</td></tr>
   <tr><td>&nbsp</td></tr>
-
 <table>
 <tr>
   <td width="60">
-   <form action='http://localhost/JWHelper/public/teacher/backPage', method="post", enctype="multipart/form-data">
-      <input type="hidden" name="lessonID", value="{{ $group->lessonID}}">
-      <input type="hidden" name="backPage", value="{{ $group->backPage}}">
-      <button type="submit" class="btn btn-info" id="submit-change" >返回</button>
-    </form>
+  <a href="http://localhost/JWHelper/public/student/myGroups"><input type="button" class="btn  btn-primary" value="返回"></input></a>
   </td>
 </tr>
 </table>
