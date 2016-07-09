bd5a5edf18f6225d5e8a20aa060b6f531ab73713
Leo Peng <pengzimaobuaa@163.com>
2016-07-09 15:26:58 +0800

    添加团队申请课程界面

---
+++ resources/views/view/student/applyCourse.blade.php
@@ -0,0 +1,52 @@
+@extends('view.template.student_layout')
+
+@section('main_panel')
+    @parent
+    <?php
+            $groupID = '1';
+            $groupName = '一颗赛艇';
+            $lessonList = [
+                ['lessonID' => '001', 'lessonName' => '数据库结构', 'teacherName' => '黄坚', 'semester' => '2013春季'],
+                ['lessonID' => '001', 'lessonName' => '数据库结构', 'teacherName' => '黄坚', 'semester' => '2013春季'],
+                ['lessonID' => '001', 'lessonName' => '数据库结构', 'teacherName' => '黄坚', 'semester' => '2013春季'],
+                ['lessonID' => '001', 'lessonName' => '数据库结构', 'teacherName' => '黄坚', 'semester' => '2013春季'],
+                ['lessonID' => '001', 'lessonName' => '数据库结构', 'teacherName' => '黄坚', 'semester' => '2013春季'],
+            ];
+    ?>
+    <h1 class="page-header">申请课程</h1>
+    <h2 class="sub-header"><small>团队名称: {{$groupName}}</small></h2>
+    <table class="table">
+        <thead>
+            <tr>
+                <th style="color:#55595c;background-color:#eceeef">课程编号</th>
+                <th style="color:#55595c;background-color:#eceeef">课程名称</th>
+                <th style="color:#55595c;background-color:#eceeef">授课教师</th>
+                <th style="color:#55595c;background-color:#eceeef">学期</th>
+                <th style="color:#55595c;background-color:#eceeef"></th>
+            </tr>
+        </thead>
+        <tbody>
+        @foreach($lessonList as $lesson)
+            <tr>
+                <th class="scope">{{$lesson['lessonID']}}</th>
+                <td>{{$lesson['lessonName']}}</td>
+                <td>{{$lesson['teacherName']}}</td>
+                <td>{{$lesson['semester']}}</td>
+                <td><button class="btn btn-success">申请课程</button></td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
+@endsection
+
+@section('bodyJS')
+    @parent
+    <script>
+        $(function () {
+            // 更改sidebar的样式, 使当前页面显示为active
+            $("#group").click();
+            $(".nav-sidebar>li").removeClass("active");
+            $("#myGroup").addClass("active");
+        });
+    </script>
+@endsection
