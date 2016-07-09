db52265f7ce084cda2fb62af4897c901207f0cb0
Leo Peng <pengzimaobuaa@163.com>
2016-07-09 10:53:43 +0800

    完成学生团队申请加入, 审核功能

--- resources/views/view/student/checkGroup.blade.php
+++ resources/views/view/student/checkGroup.blade.php
@@ -3,15 +3,6 @@
 @section('header', '审核团队')
 
 @section('main_content')
-    <?php
-        $ingroups = [
-                (object)['groupName' => 'Exciting', 'headName' => 'Leo', 'maxPeople' => '5', 'occupied' => '4', 'applyPeople' => '2'],
-                (object)['groupName' => 'Exciting', 'headName' => 'Leo', 'maxPeople' => '5', 'occupied' => '4', 'applyPeople' => '2'],
-                (object)['groupName' => 'Exciting', 'headName' => 'Leo', 'maxPeople' => '5', 'occupied' => '4', 'applyPeople' => '2'],
-                (object)['groupName' => 'Exciting', 'headName' => 'Leo', 'maxPeople' => '5', 'occupied' => '4', 'applyPeople' => '2'],
-                (object)['groupName' => 'Exciting', 'headName' => 'Leo', 'maxPeople' => '5', 'occupied' => '4', 'applyPeople' => '2'],
-        ];
-    ?>
     <table class="table">
         <thead>
         <tr>
@@ -24,16 +15,118 @@
         </tr>
         </thead>
         <tbody>
-        @foreach($ingroups as $group)
+        @foreach($checkList as $group)
             <tr>
-                <th>{{$group->headName}}</th>
+                <th>{{$group->studentName}}</th>
                 <th>{{$group->groupName}}</th>
                 <th>{{$group->maxPeople}}</th>
                 <th>{{$group->occupied}}</th>
-                <th><button class="btn btn-success">同意</button></th>
-                <th><button class="btn btn-danger">拒绝</button></th>
+                <th><button class="btn btn-success agree" data-group-id="{{$group->groupID}}" data-student-id="{{$group->studentID}}">同意</button></th>
+                <th><button class="btn btn-danger disagree" data-group-id="{{$group->groupID}}" data-student-id="{{$group->studentID}}">拒绝</button></th>
             </tr>
         @endforeach
         </tbody>
     </table>
+@endsection
+
+@section('bodyJS')
+    @parent
+    <script>
+        var baseURL = 'http://localhost/JWHelper/public/student';
+        $(function () {
+             $(".agree").click(function () {
+                 var url = baseURL + '/check';
+                 var data = {
+                     'groupID': $(this).data('groupId'),
+                     'studentID': $(this).data('studentId'),
+                     'agree': 1
+                 };
+                 $.ajax({
+                     url: url,
+                     type: "POST",
+                     data: data,
+                     dataType: "json",
+                     success: function(data) {
+                         if (data['status'] == 1) {
+                             BootstrapDialog.show({
+                                 title: '操作成功,已经同意该申请',
+                                 type: BootstrapDialog.TYPE_SUCCESS,
+                                 buttons: [
+                                     {
+                                         label: '关闭',
+                                         action: function(dialogItself) {
+                                             dialogItself.close();
+                                             location.reload();
+                                         }
+                                     }
+                                 ]
+                             });
+                         } else {
+                             BootstrapDialog.show({
+                                 title: '操作失败',
+                                 message: '请再次进行操作',
+                                 type: BootstrapDialog.TYPE_SUCCESS,
+                                 buttons: [
+                                     {
+                                         label: '关闭',
+                                         action: function(dialogItself) {
+                                             dialogItself.close();
+                                             location.reload();
+                                         }
+                                     }
+                                 ]
+                             });
+                         }
+                     }
+                 });
+             });
+
+            $(".disagree").click(function () {
+                var url = baseURL + '/check';
+                var data = {
+                    'groupID': $(this).data('groupId'),
+                    'agree': 0
+                };
+                $.ajax({
+                    url: url,
+                    type: "POST",
+                    data: data,
+                    dataType: "json",
+                    success: function(data) {
+                        if (data['status'] == 1) {
+                            BootstrapDialog.show({
+                                title: '操作成功, 已经拒绝该申请',
+                                type: BootstrapDialog.TYPE_SUCCESS,
+                                buttons: [
+                                    {
+                                        label: '关闭',
+                                        action: function(dialogItself) {
+                                            dialogItself.close();
+                                            location.reload();
+                                        }
+                                    }
+                                ]
+                            });
+                        } else {
+                            BootstrapDialog.show({
+                                title: '操作失败',
+                                message: '请再次进行操作',
+                                type: BootstrapDialog.TYPE_SUCCESS,
+                                buttons: [
+                                    {
+                                        label: '关闭',
+                                        action: function(dialogItself) {
+                                            dialogItself.close();
+                                            location.reload();
+                                        }
+                                    }
+                                ]
+                            });
+                        }
+                    }
+                });
+            });
+
+        });
+    </script>
 @endsection
