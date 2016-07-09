d14a0042cac9b1226884739e13f06595ff6deac6
joejohnson0 <joelau94@gmail.com>
2016-07-09 15:29:33 +0800

    教师功能列表跳转关系

--- resources/views/view/template/teacher_layout.blade.php
+++ resources/views/view/template/teacher_layout.blade.php
@@ -1,5 +1,9 @@
 @extends('view.template.layout')
 
+<?php
+    $baseURL = 'http://localhost/JWHelper/public/teacher';
+?>
+
 @section('sidebar')
 	@parent
     <ul class="nav nav-sidebar">
@@ -7,10 +11,26 @@
     </ul>
     @if(Session::has('lessonID'))
     <ul class="nav nav-sidebar">
-      <li id="resource"><a href="http://localhost/JWHelper/public/template/no-page"><span class="glyphicon glyphicon-list-alt" aria-hidden="true"></span> 资源列表</a></li>
-      <li id="homework"><a href="http://localhost/JWHelper/public/teacher/homework"><span class="glyphicon glyphicon-list" aria-hidden="true"></span> 作业列表</a></li>
-      <li id="team"><a href="http://localhost/JWHelper/public/template/no-page"><span class="glyphicon glyphicon-th-list" aria-hidden="true"></span> 团队列表</a></li>
+      <li id="resource"><a href="http://localhost/JWHelper/public/teacher/resourcesList"><span class="glyphicon glyphicon-list-alt" aria-hidden="true"></span> 资源列表</a></li>
+      <li id="homework"><a href="http://localhost/JWHelper/public/teacher/thomeworksList"><span class="glyphicon glyphicon-list" aria-hidden="true"></span> 作业列表</a></li>
+      <li id="group"><a href="#"><span class="glyphicon glyphicon-user" aria-hidden="true"></span> 学生团队</a></li>
+      <li id="groupsInList" class="displayNone"><a href="{{$baseURL. '/groupsInList'}}">已加入的团队</a><li>
+      <li id="groupsIOList" class="displayNone"><a href="{{$baseURL. '/groupsIOList'}}">待审核的团队</a><li>
+      <li id="groupsOutList" class="displayNone"><a href="{{$baseURL. '/groupsOutList'}}">审核被拒团队</a><li>
       <li id="chat"><a href="http://localhost/JWHelper/public/template/no-page"><span class="glyphicon glyphicon-comment" aria-hidden="true"></span> 在线交流</a></li>
     </ul>
     @endif
+@endsection
+
+@section('bodyJS')
+    @parent
+    <script>
+        $(function () {
+            $("#group").click(function () {
+                $("#groupsInList").toggleClass("displayNone");
+                $("#groupsIOList").toggleClass("displayNone");
+                $("#groupsOutList").toggleClass("displayNone");
+            });
+        });
+    </script>
 @endsection
