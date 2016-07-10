8e8947c26ca4b43fae84cbcdf4923c25e64734e9
wangdong <buaawangdong@163.com>
2016-07-10 15:15:17 +0800

    修复了教师给学生作业评分的bug

--- resources/views/view/teacher/shomeworkRate.blade.php
+++ resources/views/view/teacher/shomeworkRate.blade.php
@@ -1,3 +1,9 @@
+
+<?php
+$thomework = $thomework[0];
+$shomework = $shomework[0];
+?>
+
 @extends('view.template.teacher_layout')
 @section('headjs')
 <script type="text/javascript">
@@ -8,9 +14,9 @@
 				url : "http://localhost/JWHelper/public/teacher/shomeworkRate" ,
 				dataType : 'json',
 				data : {
-
+					shomeworkID : {{ $shomework->shomeworkID }},
 					grade : $('#grade').val() ,
-					comment : $('#comment').val() ,
+					comment : $('#comment').val()
 				},	
 				success : function(data){
 					if (data.status == 1){
@@ -24,11 +30,6 @@
     });
 </script>
 @endsection
-
-<?php
-$thomework = $thomework[0];
-$shomework = $shomework[0];
-?>
 
 @section('main_panel')
 
@@ -95,7 +96,6 @@ $shomework = $shomework[0];
 		<h3>作业评语</h3></label>
 		<textarea style="width:80%" placeholder="请写下您的评语" class="form-control" name="comment" id="comment" rows="4"></textarea>
 </div>
-	<input type="hidden" name='shomeworkID' value="{{ $shomework->shomeworkID}}">
 	<div class="col-md-2">
 	<button id="submit-grade" type="submit" class="btn btn-primary">提交</button>
 	</div>
@@ -110,15 +110,11 @@ $shomework = $shomework[0];
 		<h3>作业评语</h3></label>
 		<textarea style="width:80%" placeholder="请写下您的评语" class="form-control" name="comment" id="comment"  rows="4">{{$shomework->comment}}</textarea>
 </div>
-	<input type="hidden" name='shomeworkID' value="{{ $shomework->shomeworkID}}">
 	<div class="col-md-2">
 	<button id="submit-grade" type="submit" class="btn btn-primary">提交</button>
 	</div>
 
 @endif
-
-
-    <input type="hidden" name="thomeworkID" value="{{ $thomework->thomeworkID}}">
     <div class="col-md-2">
     <button  type="submit" class="btn btn-success">返回</button>
     </div>
