d37e9f3cbc13cd1ffc8537ba03b4caec72a2dc98
joejohnson0 <joelau94@gmail.com>
2016-07-10 18:38:24 +0800

    批量下载

--- resources/views/view/template/admin_layout.blade.php
+++ resources/views/view/template/admin_layout.blade.php
@@ -9,8 +9,8 @@
       <li id="semesterInfo"><a href="http://localhost/JWHelper/public/admin/semester_info"><span class="glyphicon glyphicon-cog" aria-hidden="true"></span> 学期信息设置</a></li>
     </ul>
     <ul class="nav nav-sidebar">
-      <li id="teacherList"><a href="http://localhost/JWHelper/public/template/no-page"><span class="glyphicon glyphicon-th-list" aria-hidden="true"></span> 教师列表</a></li>
-      <li id="studentList"><a href="http://localhost/JWHelper/public/template/no-page"><span class="glyphicon glyphicon-th-list" aria-hidden="true"></span> 学生列表</a></li>
+      <li id="teacherList"><a href="http://localhost/JWHelper/public/admin/teachersList"><span class="glyphicon glyphicon-th-list" aria-hidden="true"></span> 教师列表</a></li>
+      <li id="studentList"><a href="http://localhost/JWHelper/public/admin/studentsList"><span class="glyphicon glyphicon-th-list" aria-hidden="true"></span> 学生列表</a></li>
       <li id="importTeacher"><a href="http://localhost/JWHelper/public/admin/uploadTeacherPage"><span class="glyphicon glyphicon-import" aria-hidden="true"></span> 导入教师信息</a></li>
       <li id="importStudent"><a href="http://localhost/JWHelper/public/admin/uploadStudentPage"><span class="glyphicon glyphicon-import" aria-hidden="true"></span> 导入学生信息</a></li>
     </ul>
